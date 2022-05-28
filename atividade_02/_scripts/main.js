570db5d0b112a728f39c057c3369c4255f224a6d
Nethobr <leezando.neto@gmail.com>
2022-05-27 23:33:36 -0300

    27/05/2022

--- atividade_02/_scripts/main.js
+++ atividade_02/_scripts/main.js
@@ -117,14 +117,129 @@ divDoTre = () => {
     if ((num % 3) == 0 && (num % 2) == 0) {
         param = "divisível por 2 e 3"
         res(num, param);
-    } else if ((num % 2) == 0) {
-        param = "divisível por 2"
-        res(num, param);
-    } else if ((num % 3) == 0) {
-        param = "divisível por 3"
-        res(num, param);
     } else {
         param = "não é divisível por 2 e 3"
         res(num, param);
     }   // Fim if-else
 }   // Fim divDoTre
+
+// Atividade 6
+divDoSet= () => {
+    //VAR'S
+    var num = Number(document.getElementById("num").value);
+
+    // Função que retorna o resultado
+    res = (num, param) => {
+        document.getElementById("resultado").innerHTML = ("O número " + num + " " + param + ".");
+    }   // Fim res
+
+    if ((num % 2) == 0 && (num % 7) == 0) {
+        param = "divisível por 2 e 7"
+        res(num, param);
+    } else if ((num % 2) == 0) {
+        param = "divisível por 2"
+        res(num, param);
+    } else if ((num % 7) == 0) {
+        param = "divisível por 7"
+        res(num, param);
+    } else {
+        param = "não é divisível por 2 e 7"
+        res(num, param);
+    }   // Fim if-else
+}   // Fim divDoTre
+
+// Atividade 7
+diaSemana = () => {
+    //Var's
+    var dia = Number(document.getElementById("dia").value);
+
+    show = (val) => { return document.getElementById("resultado").innerHTML = ("O dia é " + val);}
+
+    switch (dia){
+        case 1:
+            show("Domingo");
+        break;
+        case 2:
+            show("Segunda-feira");
+        break;
+        case 3:
+            show("Terça-feira");
+        break;
+        case 4:
+            show("Quarta-feira");
+        break;
+        case 5:
+            show("Quinta-feira");
+        break;
+        case 6:
+            show("Sexta-feira");
+        break;
+        case 7:
+            show("Sábado");
+        break;
+
+        default:
+            document.getElementById("resultado").textContent = "Dia não registrado."
+        break;
+    }
+}   // Fim diasSemana;
+
+// Atividade 8
+calcPar = () => {
+    var soma = Number(0);
+
+    for (var i = 0; i <= 20; i++)
+    {
+        if ((i % 2) == 0)
+        {
+            soma += i;
+        }
+    }   // Fim for
+
+    document.getElementById("resultado").innerHTML = (soma);
+}   // Fim calcPar
+
+// Atividade 9
+tabuada = () => {
+
+    var num = Number(document.getElementById("num").value);
+
+    var resposta = document.getElementById('resposta');
+
+    var tabuada = '';
+  
+    for(var i=1; i<=10 ; i++)
+    {
+        tabuada += num + " x " + i + " = " + num*i + ".<br>";
+    
+        resposta.innerHTML = tabuada
+    }
+    document.getElementById("resNum").innerHTML = ("Tabuada do " + num + ".");
+}   // calcTabuada
+
+// Atividade 10
+fatorial = () => {
+
+    var num = Number(document.getElementById("num").value);
+
+    var resposta = document.getElementById('resposta');
+
+    var temp = Number(1);
+
+    var fat = '';
+  
+    for(var i = num; i > 0 ; i--)
+    {
+        if (i != 1){
+            fat += i + " * ";
+        }
+        else {
+            fat += i + " = ";
+        }
+    
+        temp =+ temp * i;
+        resposta.innerHTML = fat
+    }
+    document.getElementById("resNum").innerHTML = ("Fatorial de " + num + ".");
+    document.getElementById("final").innerHTML = (temp + " e 0! = 1.");
+}   // fatorial
